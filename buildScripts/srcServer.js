3663c1378556d983b96cc13c2cce72f851a6fb3f
Tyler Campbell <tyler_campbell@manulife.ca>
2021-08-05 16:33:47 -0400

    second commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,10 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
+app.get("/users", (req, res) => {
+  res.json(["tyler", "michael", "campbell"]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
